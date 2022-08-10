f6ddc7507c103b8bcda9d53dd1bc29d47cb89034
John Emilio <jl.emilio1@gmail.com>
2022-08-10 17:47:45 -0400

    Added edit page, delete functionality, views for single story

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const methodOverride = require('method-override')
 const { engine } = require('express-handlebars')
 const connectDB = require('./config/db');
 const passport = require('passport');
@@ -25,17 +26,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 // Logging
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'))
 };
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 
 // Handlebars
 app.engine('.hbs', engine({
-    helpers: { formatDate, stripTags, truncate, editIcon },
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
     defaultLayout: 'main', extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
